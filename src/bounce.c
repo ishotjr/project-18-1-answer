5703a073a4f80dcdd8f98e1db50d7fee86a93ee0
programming-pebble-in-c <jipping+ppic@hope.edu>
2015-12-08 14:33:48 -0500

    Small fixes

--- src/bounce.c
+++ src/bounce.c
@@ -1,33 +1,45 @@
+//----------------------------------------------------------------------------------
+//
+// Project 3.1 from "Programming Pebble in C"
+// 
+// Mike Jipping, December 2015
+
 #include <pebble.h>
 
 #define ACCEL_STEP_MS 50
 
+// These declarations are necessary to run on the watch
 static Window *window;
 static Layer *bouncy_layer;
 static GRect window_frame;
 
+// These are the declarations that configure the ball!
 static int position_x, position_y;
 static int x_velocity, y_velocity;
 static int ball_radius = 20;
 
+// The select button starts the ball rolling (so to speak)
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
     position_x = 72;
     position_y = 25;
     x_velocity = y_velocity = 6;
 } 
 
+// The up button increases the ball size and speed.
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
     ball_radius ++;
     x_velocity ++;
     y_velocity ++;
 }
 
+// The down button decreases the ball's size and speed
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
     ball_radius --;
     x_velocity --;
     y_velocity --;
 }
 
+// This function moves the ball every time the timer goes off
 static void bounce_the_ball() {
     if ((position_x - ball_radius < 0)
         || (position_x + ball_radius > window_frame.size.w)) {
@@ -40,7 +52,8 @@ static void bounce_the_ball() {
     position_y = position_y + y_velocity;
     position_x = position_x + x_velocity;
 }
-    
+  
+//-------------------------------------------------------------------------------
 
 static void bouncy_layer_update_callback(Layer *me, GContext *ctx) { 
     if (position_x == -1) return;
